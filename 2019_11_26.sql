f5c8670e387889ce8bb3353ba604bacf9ee9e05e
hh.joo <joohhcse@gmail.com>
2019-11-27 12:46:56 +0900

    "문자열 <-> 날짜변환 형변환(묵시적, 명시적) 실행계획" "숫자 문자열 변환 NULL 처리  .NVL (*)  .NVL2  .NULLIF  .COALESCE (+)" "NULL처리 함수 실습
    조건제어
    .CASE
    .DECODE 함수"

--- 2019_11_26.sql
+++ 2019_11_26.sql
@@ -130,12 +130,15 @@ FROM emp;
 SELECT empno, ename, comm, NVL(comm, -1) nvl_comm
 FROM emp;
 
+--study test
+SELECT empno, comm, nvl2(comm, /*NOT NULL*/ 000000, /*NULL*/ 999999) nvl_test
+FROM emp;
+
 --NVL2(expr1, expr2, expr3)
 --expr1 IS NOT NULL expr2 리턴
 --expr1 IS NULL expr3 리턴
 SELECT empno, ename, comm, NVL2(comm, 1000, -500) nvl_comm
 FROM emp;
-
 
 --NULLIF(expr1, expr2)
 --expr1 = expr2 NULL을 리턴
@@ -161,12 +164,9 @@ SELECT empno, ename, mgr, NVL(mgr, 9999) mgr_n,
     COALESCE(mgr, 9999) mgr_n_2
 FROM emp;
 
-
 --fn5
 SELECT *
 FROM users;
-
-
 
 SELECT userid, usernm, reg_dt, n_reg_dt
 FROM
@@ -174,13 +174,15 @@ FROM
 FROM users)
 WHERE e BETWEEN 2 AND 5;
 
-
-
 -->fn5 > Sol
 SELECT userid, usernm, reg_dt, COALESCE(reg_dt, SYSDATE) n_reg_dt
 FROM users
 WHERE userid NOT IN ('brown');
 
+SELECT *
+FROM users;
+
+DESC users;
 
 --condition
 --case
@@ -215,21 +217,80 @@ SELECT empno, ename, job, sal,
                              sal) bonus
 FROM emp;
 
+SELECT empno, ename, 
+    DECODE(deptno, 10, 'ACCOUNTING', 
+                    20, 'RESEARCH', 
+                    30, 'SALES', 
+                    40, 'OPERATIONS',
+                    'DDIT') dname
+FROM emp;
+
+
+--condition 실습 cond1
+--emp 테이블을 이용하여 deptno에 따라 부서명으로 변경해서
+--다음과 같이 조회하는 쿼리를 작성하세요
+SELECT empno, ename,
+    CASE
+        WHEN deptno = 10 THEN 'ACCOUNTING'
+        WHEN deptno = 20 THEN 'RESEARCH'
+        WHEN deptno = 30 THEN 'SALES'
+        WHEN deptno = 40 THEN 'OPERATIONS'
+        ELSE 'DDIT'
+    END dname
+FROM emp;
+
+SELECT *
+FROM emp;
+
+
+--condition 실습 cond2
+--emp 테이블을 이용하여 hiredate에 따라 올해 건강보험 검진대상자인지
+--조회하는 쿼리를 작성하세요
+--(생년을 기준으로 하나 여기서는 입사년도를 기준으로 한다)
+
+--1. 올해년도가 짝수/홀수 인지
+--2. hiredate에서 입사년도가 짝수/홀수 인지
+
+--1. TO_CHAR(SYSDATE, 'YYYY')
+--> 올해년도 구분 ( 0:짝수, 1:홀수)
+SELECT MOD(TO_CHAR(SYSDATE, 'YYYY'), 2) 
+FROM dual;
+
+--2
+SELECT empno, ename, 
+    CASE
+        WHEN MOD(TO_CHAR(hiredate, 'YYYY'), 2) =
+             MOD(TO_CHAR(SYSDATE, 'YYYY', 2)
+        THEN '건강검진 대상자'
+        ELSE '건강검진 비대상자'
+    END contact_to_doctor
+FROM emp;
+
+--2.
+--내년도 건강검진 대상자를 조회하는 쿼리를 작성해보세요
+--2020
+SELECT empno, ename, 
+    CASE
+        WHEN MOD(TO_CHAR(hiredate, 'YYYY'), 2) =
+             MOD(TO_CHAR(SYSDATE, 'YYYY'), 2)
+        THEN '건강검진 대상자'
+        ELSE '건강검진 비대상자'
+    END contact_to_doctor
+FROM emp;
 
 
 
+--SELECT empno, ename, 
+--    CASE 
+--        WHEN MOD(TO_CHAR(hiredate, 'YYYY'), 2) = 1 THEN '건강검진 대상자'
+--        WHEN MOD(TO_CHAR(hiredate, 'YYYY'), 2) = 0 THEN '건강검진 비대상자'
+--        ELSE 'DEFAULT'
+--    END
+--FROM emp;
 
 
-
-
-
-
-
-
-
-
-
-
+SELECT empno, ename, MOD(TO_CHAR(hiredate, 'YYYY'), 2) test
+FROM emp;
 
 
 
