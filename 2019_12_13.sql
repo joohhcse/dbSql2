9c989b4f4b5ac910fa519c4782316687a43a082d
hh.joo <joohhcse@gmail.com>
2019-12-13 19:33:40 +0900

    20191213 SQL

--- 2019_12_13.sql
+++ 2019_12_13.sql
@@ -172,6 +172,21 @@ FROM emp
 GROUP BY ROLLUP (job, deptno);
 
 --GROUP_AD2
+--CLERK	    10	1300
+--CLERK	    20	1900
+--CLERK	    30	950
+--CLERK	    NULL	4150
+--ANALYST	    20	6000
+--ANALYST	    NULL	6000
+--MANAGER	    10	2450
+--MANAGER	    20	2975
+--MANAGER	    30	2850
+--MANAGER	    NULL	8275
+--SALESMAN	30	7800
+--SALESMAN	NULL	7800
+--PRESIDENT	10	5000
+--PRESIDENT	NULL	5000
+--ÃÑ°è	        NULL	31225
 SELECT NVL(job, 'ÃÑ°è') job, deptno, SUM(sal + NVL(comm,0)) sal_sum
 FROM emp
 GROUP BY ROLLUP (job, deptno);
@@ -181,7 +196,23 @@ SELECT DECODE(GROUPING (job), 1, 'ÃÑ°è', 0, job) job, deptno, SUM(sal + NVL(comm
 FROM emp
 GROUP BY ROLLUP (job, deptno);
 
+
 --GROUP_AD2 - 1
+--CLERK	    10	1300
+--CLERK	    20	1900
+--CLERK	    30	950
+--CLERK	    ¼Ò°è	4150
+--ANALYST 	20	6000
+--ANALYST 	¼Ò°è	6000
+--MANAGER	    10	2450
+--MANAGER	    20	2975
+--MANAGER	    30	2850
+--MANAGER	    ¼Ò°è	8275
+--SALESMAN	30	7800
+--SALESMAN	¼Ò°è	7800
+--PRESIDENT	10	5000
+--PRESIDENT	¼Ò°è	5000
+--ÃÑ	        ¼Ò°è	31225
 SELECT DECODE(GROUPING (job), 1, 'ÃÑ', 0, job) job,
     DECODE(GROUPING (deptno), 1, '¼Ò°è', 0, deptno) deptno,
     SUM(sal + NVL(comm,0)) sal_sum,
@@ -191,13 +222,27 @@ FROM emp
 GROUP BY ROLLUP (job, deptno);
 
 --CASE ¹® »ç¿ë(GROUP_AD2 - 1)
+--CLERK	    10	    CLERK	    10
+--CLERK	    20	    CLERK	    20
+--CLERK	    30	    CLERK	    30
+--CLERK	    NULL	CLERK	    ¼Ò°è
+--ANALYST	20  	ANALYST	    20
+--ANALYST	NULL	ANALYST	    ¼Ò°è
+--MANAGER	10	    MANAGER	    10
+--MANAGER	20	    MANAGER	    20
+--MANAGER	30	    MANAGER	    30
+--MANAGER	NULL	MANAGER	    ¼Ò°è
+--SALESMAN	30  	SALESMAN	30
+--SALESMAN	NULL    SALESMAN	¼Ò°è
+--PRESIDENT	10	    PRESIDENT	10
+--PRESIDENT	NULL	PRESIDENT	¼Ò°è
+--NULL	    NULL	ÃÑ	        °è
 SELECT job, deptno, DECODE(GROUPING (job), 1, 'ÃÑ', 0, job) job,
     CASE 
         WHEN deptno IS NULL AND job IS NULL THEN '°è'
         WHEN deptno IS NULL AND job IS NOT NULL THEN '¼Ò°è'
         ELSE '' || deptno -- inconsistent datatype : expect CHAR got NUMBER >> deptno ¸¸ ÀÛ¼ºÇßÀ»¶§
-        --ELSE TO_
-        CHAR(deptno)  -- ¶Ç´Â
+        --ELSE TO_CHAR(deptno)  -- ¶Ç´Â
     END,
     DECODE(GROUPING (deptno), 1, '¼Ò°è', 0, deptno) deptno,
     SUM(sal + NVL(comm,0)) sal_sum,
@@ -207,6 +252,19 @@ FROM emp
 GROUP BY ROLLUP (job, deptno);
 
 --GROUP_AD3
+--10	CLERK	    1300
+--10	MANAGER 	2450
+--10	PRESIDENT	5000
+--10	NULL	    8750
+--20	CLERK	    1900
+--20	ANALYST 	6000
+--20	MANAGER 	2975
+--20	NULL	    10875
+--30	CLERK	    950
+--30	MANAGER 	2850
+--30	SALESMAN	7800
+--30	NULL    	11600
+--NULL	NULL    	31225
 SELECT deptno, job, SUM(sal + NVL(comm,0)) sal_sum
 FROM emp
 GROUP BY ROLLUP (deptno, job);
@@ -224,8 +282,21 @@ UNION ALL
 SELECT NULL, NULL, SUM(sal + NVL(comm,0)) sal_sum
 FROM emp;
 
-
 --GROUP_AD4
+--ACCOUNTING	NULL	    8750
+--ACCOUNTING	PRESIDENT	5000
+--ACCOUNTING	MANAGER 	2450
+--ACCOUNTING	CLERK	    1300
+--RESEARCH	NULL	    10875
+--RESEARCH	MANAGER	    2975
+--RESEARCH	ANALYST	    6000
+--RESEARCH	CLERK	    1900
+--SALES   	NULL    	11600
+--SALES	    SALESMAN	7800
+--SALES	    MANAGER 	2850
+--SALES   	CLERK	    950
+--NULL	    NULL	    31225
+
 --DB½Ü ´ä
 SELECT dept.dname, a.job, a.sal_sum
 FROM
@@ -246,7 +317,21 @@ SELECT *
 FROM dept;
 
 --GROUP_AD5
+--ACCOUNTING	NULL    	8750
+--ACCOUNTING	PRESIDENT	5000
+--ACCOUNTING	MANAGER	    2450
+--ACCOUNTING	CLERK	    1300
+--RESEARCH	    NULL	    10875
+--RESEARCH	    MANAGER	    2975
+--RESEARCH	    ANALYST	    6000
+--RESEARCH  	CLERK	    1900
+--SALES	        NULL	    11600
+--SALES	        SALESMAN	7800
+--SALES	        MANAGER 	2850
+--SALES	        CLERK	    950
+--ÃÑ°è	        NULL	    31225
 
+--DB½Ü ´ä
 SELECT NVL(dept.dname, 'ÃÑ°è') dname, a.job, a.sal_sum
 FROM
     (SELECT deptno, job,
@@ -254,6 +339,7 @@ FROM
     FROM emp
     GROUP BY ROLLUP(deptno, job)) a, dept
 WHERE a.deptno = dept.deptno(+);
+--DB½Ü ´ä
 
 SELECT 
     CASE 
